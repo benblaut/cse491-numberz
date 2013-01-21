a0ca2fd02842ad80d51730275540c75d06df5cff
Ben Blaut <blautben@arctic.cse.msu.edu>
2013-01-21 14:54:10 -0500

    finished homework

---
+++ sieve_iter/sieve_iter.py
@@ -0,0 +1,21 @@
+class sieve(object):
+    def __init__(self):
+		self._primeslist = [2]
+
+    def __iter__(self):
+		return self
+
+    def _is_prime(self, primes, n):
+		for i in primes:
+	   		if n % i == 0:
+				return False
+		return True
+
+    def next(self):
+		self.start = self._primeslist[-1] + 1
+		while 1:
+			if self._is_prime(self._primeslist, self.start):
+				self._primeslist.append(self.start)
+				return self.start
+			self.start += 1
+
